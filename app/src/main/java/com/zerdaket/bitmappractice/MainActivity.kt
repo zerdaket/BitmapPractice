f49add45d3a16656bb50b4564fb8d6f1bd5ffedc
zerdaket <zerdaket@gmail.com>
2020-09-16 23:58:40 +0800

    新增绘制两个纯色图形

--- app/src/main/java/com/zerdaket/bitmappractice/MainActivity.kt
+++ app/src/main/java/com/zerdaket/bitmappractice/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.zerdaket.bitmappractice
 
+import android.graphics.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.zerdaket.bitmappractice.utils.BitmapUtils
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val paint = Paint()
+        paint.isAntiAlias = true
+        val src = Bitmap.createBitmap(500, 500, Bitmap.Config.ARGB_8888)
+        val srcCanvas = Canvas(src)
+        paint.color = Color.YELLOW
+        srcCanvas.drawRect(Rect(100, 100, 300, 200), paint)
+        val dst = Bitmap.createBitmap(400, 400, Bitmap.Config.ARGB_8888)
+        val dstCanvas = Canvas(dst)
+        paint.color = Color.BLUE
+        dstCanvas.drawCircle(200f, 150f, 100f, paint)
+
+        val bitmap = BitmapUtils.composeBitmap(src, dst, PorterDuff.Mode.SRC_IN)
+        imageView.setImageBitmap(bitmap)
     }
 }
--- app/src/main/java/com/zerdaket/bitmappractice/utils/BitmapUtils.kt
+++ app/src/main/java/com/zerdaket/bitmappractice/utils/BitmapUtils.kt
@@ -12,18 +12,22 @@ object BitmapUtils {
     fun composeBitmap(src: Bitmap, dst: Bitmap, mode: PorterDuff.Mode): Bitmap {
         val width = max(src.width, dst.width)
         val height = max(src.height, dst.height)
-        val srcBitmap = src.copy(Bitmap.Config.ARGB_8888, false)
-        val dstBitmap = dst.copy(Bitmap.Config.ARGB_8888, false)
+        val srcBitmap = src.copy(Bitmap.Config.ARGB_8888, true)
+        val dstBitmap = dst.copy(Bitmap.Config.ARGB_8888, true)
         val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
         val canvas = Canvas(bitmap)
 
         val paint = Paint()
         paint.isAntiAlias = true
 
+        canvas.saveLayer(null, null)
+
         canvas.drawBitmap(srcBitmap, 0f, 0f, paint)
         paint.xfermode = PorterDuffXfermode(mode)
         canvas.drawBitmap(dstBitmap, 0f, 0f, paint)
         paint.xfermode = null
+
+        canvas.restore()
 
         return dstBitmap
     }
